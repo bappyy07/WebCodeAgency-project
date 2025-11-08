1ed6e1a286d9a1da4bfc50e60dbe509754324375
arnysamar@gmail.com <arnysamar@gmail.com>
2025-11-08 20:39:51 +0400

    done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,11 +1,12 @@
 import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID
+  apiKey: "AIzaSyBrRcHGw8glHNjDE70aCefnM4Y4_wIBWmM",
+  authDomain: "webagency-pro-73a1b.firebaseapp.com",
+  projectId: "webagency-pro-73a1b",
+  storageBucket: "webagency-pro-73a1b.firebasestorage.app",
+  messagingSenderId: "579548677859",
+  appId: "1:579548677859:web:e0f4d8baba67ac0c9f27d6"
 };
 
 // Initialize Firebase
